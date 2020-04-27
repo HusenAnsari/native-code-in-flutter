26984ca2583fd06ab57e995383b9c58569ea3367
Husen Ansari <husenansari07@gmail.com>
2020-04-27 21:02:08 +0530

    Native android code in flutter

--- android/app/src/main/java/com/example/myapp/MainActivity.java
+++ android/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.myapp;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
 
-import androidx.annotation.NonNull;
+import java.util.Objects;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+    new MethodChannel(Objects.requireNonNull(getFlutterEngine()).getDartExecutor().
+            getBinaryMessenger(), "course.flutter.dev/battery").setMethodCallHandler(
+            new MethodChannel.MethodCallHandler() {
+              @Override
+              public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                if(methodCall.equals("getBatteryLevel")){
+                  int batteryLevel = getBatteryLevel();
+                  if (batteryLevel != -1){
+                      result.success(batteryLevel);
+                  }else {
+                    result.error("UNAVAILABLE","Could not fetch battery level.", null);
+                  }
+                }else {
+                  result.notImplemented();
+                }
+              }
+            }
+    );
+  }
+  // here -1 = value not get
+  private int getBatteryLevel(){
+    int batteryLevel = -1;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+      BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+      assert batteryManager != null;
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    }else {
+      Intent intent = new ContextWrapper(getApplicationContext()).registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      assert intent != null;
+      batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    }
+    return batteryLevel;
   }
 }
